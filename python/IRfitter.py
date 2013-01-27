5a9a0d51c07f8a5a19fef8ef01bf90ed9528ccdb
David-Estevez <david.estevez.fdez@gmail.com>
2013-01-27 19:04:26 +0100

    Parameters calculation works.

--- python/IRfitter.py
+++ python/IRfitter.py
@@ -5,12 +5,20 @@
 
 # Loading dependencies:
 import sys
+
+# Import Qt4
 try:
 	from PyQt4 import QtGui, QtCore
 except ImportError:
 	print "Error: PyQt4 not installed."
 	exit()
+# Import NumPy
+try:
+	import numpy as np	
 
+except ImportError:
+	print "Error: NumPy is not installed"
+	exit()
 
 class IRfitterWindow( QtGui.QMainWindow ):
 	""" Main Window of the IRfitter program """
@@ -74,6 +82,12 @@ class IRfitterWindow( QtGui.QMainWindow ):
 		self.show()
 
 	def loadFile(self):
+		"""
+			Action to perform when 'load' button is pressed.
+			--------------------------------------------------------
+			Loads data from the given path, and calculates the 
+			a and b parameters, which are later shown on the window.
+		"""
 		file_path = QtGui.QFileDialog.getOpenFileName(self, 'Open file', './', '*.txt' )
 		a, b = self.calculateParameters( file_path )
 		self.statusBar().showMessage( 'Calculations done.')
@@ -82,15 +96,16 @@ class IRfitterWindow( QtGui.QMainWindow ):
 
 	def linkFunction(self, function):
 		"""
-		Assigns the function to call for calculating the parameters.
-		This function sould take as parameter one file path and return
-		the values of a and b. 
+			Assigns the function to call for calculating the parameters.
+			This function should take as parameter one file path and return
+			the values of a and b. 
 		"""
 		self.calculateParameters = function
 
 	def genResultText(self, a, b):
 		""" 
-		Generates the text to display in the label. If a and b are not known yet, it displays '??'
+			Generates the text to display in the label. If a and b are not 
+			known yet, it displays '??'
 		"""
 		if a == None:
 			a = '??'
@@ -101,18 +116,57 @@ class IRfitterWindow( QtGui.QMainWindow ):
 
 
 def calculate( file_path):
-	a, b = 0, 0
-	try:
-		import numpy as np	
-		data = np.loadtxt( str(file_path) )
-		print data
+	"""
+		This function should take as parameter one file path and return 
+		the values of a and b. 
+	"""
+	# Load data from given file:
+	data = np.loadtxt( str(file_path) )
 
-	except ImportError:
-		print "Error: NumPy is not installed"
+	# Extract data from loaded data:
+	X = data[:, 0]	
+	y = np.vstack( data[:, 1] )	
+	m = y.size
 
+	# Transform data (log)
+	X = np.log(X)
+	y = np.log(y)
+
+	# Add a column of 1 and revert back orientation:
+	X = (np.vstack( ( np.ones(( 1, m)) , X) ) ).T
+
+	# Create initial guess:
+	theta = np.array( [ [ 0 ] , [ 0 ] ] )
+
+	# Fit the data:
+	theta = gradientDescend( X, y, theta, 0.001, 60000)
+
+	# Undo the transformation:	
+	a = np.exp( theta[0][0] )
+	b = - theta[1][0]
+ 
+	print theta
+	print a, b	
 	return a, b
 
-def main():
+""" Math functions: """
+def gradientDescend( X, y, theta, alpha, iterations):
+	m = y.size
+	for i in range( 0, iterations):
+		theta = theta - (alpha / m ) * np.dot( X.T, np.dot( X, theta) - y)
+	return theta
+
+def costFunction( X, y, theta):
+	m = y.size
+	cost = 1 / (2 * m) *  np.dot( (h(X, theta) - y).T , h(X, theta) - y )
+	return cost
+
+def h( X, theta):
+	return np.dot( X, theta)
+	
+
+""" Main function """
+def main():	
 	app = QtGui.QApplication(sys.argv)
 	win = IRfitterWindow()
 	win.linkFunction( calculate )
